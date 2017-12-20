3a813965e7d00153cab07a1222b14e0ecc7836e0
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2017-12-21 00:52:33 +0300

    Move test resource test/{currencyRatesTest.js => resources/currencyRatesTest.json}

--- currencyRatesTest.sh
+++ currencyRatesTest.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-curl -XPOST localhost:5000/webhook -d @test/currencyRatesTest.js -H "Content-Type: application/json" | json_pp 2> /dev/null
+curl -XPOST localhost:5000/webhook -d @test/resources/currencyRatesTest.json -H "Content-Type: application/json" | json_pp 2> /dev/null
