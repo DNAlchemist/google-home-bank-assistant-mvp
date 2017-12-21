4190edfc23a4600c144361b634890e0470e14409
Sergey Serdyuk <sserdyuk@alfabank.ru>
2017-12-21 14:33:27 +0300

    Merge branch 'master' of https://github.com/DNAlchemist/google-home-bank-assistant-mvp
    # Conflicts:
    #	index.js

--- convertCurrencyTest.sh
+++ convertCurrencyTest.sh
@@ -1,2 +1,2 @@
 # !/bin/bash
-curl -XPOST https://chest.one/neee -d @test/resources/convertCurrencyTest.json -H "Content-Type: application/json" | json_pp 2> /dev/null
+curl -XPOST https://chest.one/neee -d @test/resources/convertMoneyTest.json -H "Content-Type: application/json" | json_pp 2> /dev/null
