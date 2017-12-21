c9858da2c10e666772289d62fa1bc244aca688f8
Ruslan Mikhalev <RMikhalev@alfabank.ru>
2017-12-21 21:16:01 +0300

    Test

---
+++ test/currencyRatesNotFoundTest.js
@@ -0,0 +1,40 @@
+{
+   "id":"9823edc5-5c8a-4620-b170-42f4afacd1ec",
+   "timestamp":"2017-12-18T13:52:01.679Z",
+   "lang":"ru",
+   "result":{
+      "source":"agent",
+      "resolvedQuery":"Show currency",
+      "speech":"",
+      "action":"input.currency",
+      "actionIncomplete":false,
+      "parameters":{
+         "currency": ["GEB"]
+      },
+      "contexts":[
+
+      ],
+      "metadata":{
+         "intentId":"b3888014-d633-4de4-ae97-948fc67e2264",
+         "webhookUsed":"true",
+         "webhookForSlotFillingUsed":"false",
+         "intentName":"Currency Intent"
+      },
+      "fulfillment":{
+         "speech":"",
+         "messages":[
+            {
+               "type":0,
+               "speech":""
+            }
+         ]
+      },
+      "score":1
+   },
+   "status":{
+      "code":200,
+      "errorType":"success",
+      "webhookTimedOut":false
+   },
+   "sessionId":"d2c14dff-872e-46db-a718-8fa763013c18"
+}
